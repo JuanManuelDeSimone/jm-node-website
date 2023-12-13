a3ca77f96d5d9190d89603174b1ec3c75380f1af
Juan Manuel De Simone <desimonej@gmail.com>
2023-12-13 16:55:36 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenido</h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+
+app.listen(3000);
+console.log("Server on port", 3000);
+
+//Plataformas para deploy
+//Heroku
+//Render
+//Railway
